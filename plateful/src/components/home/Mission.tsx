1e4c4f77eee8e53ff9de8965d4edc1d88ed48d74
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2025-01-25 21:21:41 -0500

    new image and colour in landing page

--- plateful/src/components/home/Mission.tsx
+++ plateful/src/components/home/Mission.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 
+
+
 export default function Mission() {
   return (
-    <section className="bg-dark-green pt-[0.5rem] pb-[15rem] rounded-3xl top-[-20px]">
-      <h2 className="text-6xl text-white font-bold text-center mt-[1.5rem]">
-        Our Mission
+    <section className="bg-bg-main pt-[0.5rem] pb-[15rem] rounded-3xl top-[-20px]">
+      <h2 className="text-6xl text-black font-bold text-center mt-[1.5rem]">
+      Our Mission
       </h2>
       <div className="flex flex-row mx-[6rem] mt-[2.5rem] gap-[3rem]">
-        <div className="flex-1 border-2 border-green-bg h-[40rem] rounded-lg">
-          <img src="./partners-in-mission"/>
+      <div className="flex-1 border-2 border-white-bg h-[40rem] rounded-xl">
+      <img className="w-full h-full object-cover rounded-xl" src="/images/partners-in-mission.jpg"></img>
+      </div>
+      <div className="flex-1">
+      <div className="flex flex-col h-full p-[3.5rem]">
+        <h2 className="text-4xl text-black font-bold">Fighting food insecurity...</h2>
+        <h2 className="text-4xl text-black font-bold">One plate at a time</h2>
+        <p className="text-black text-2xl mt-16">
+        At Plateful, our mission is to combat food insecurity in Kingston by connecting grocery 
+        stores with food banks through a streamlined digital platform. Using AI-driven insights,
+         we optimize surplus food donations and volunteer delivery routes to reduce waste, improve 
+         distribution efficiency, and ensure timely support for those in need. Together, we’re building
+        a sustainable and community-driven solution to make every meal count.
+        </p>
         </div>
-        <div className="flex-1">
-        <div className="flex flex-col h-full p-[3.5rem]">
-            <h2 className="text-4xl text-white font-bold">Hello World</h2>
-            <p className="text-xl mt-[4rem]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-              nec nullaLorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur nec nullaLorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Curabitur nec nullaLorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Curabitur nec nulla
-            </p>
-          </div>
-        </div>
+      </div>
       </div>
     </section>
   );
